053ab08a77158f6699b0abbfda0eac8f98253319
michalg9 <michal_9@op.pl>
2016-03-24 14:01:16 +0100

    first commit

---
+++ installFlows.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+#set -x
+
+for i in `seq 1 $1`
+do
+	echo 'Flows installed at switch s'$i
+	sudo ovs-ofctl add-flow s$i -O OpenFlow13 priority=101,dl_type=0x88cc,actions=CONTROLLER
+	echo '------------\n'
+done
